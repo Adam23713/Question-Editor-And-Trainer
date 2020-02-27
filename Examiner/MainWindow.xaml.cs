8cf08821e84344d4cf773d2d67fb4c5646b81480
Kertész Ádám <kerteszadam@hotmail.hu>
2020-02-27 13:08:23 +0100

    Add new icons and add menu bar.

--- Examiner/MainWindow.xaml.cs
+++ Examiner/MainWindow.xaml.cs
@@ -20,9 +20,26 @@ namespace Examiner
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region WindowTitle
+        private string FileName
+        {
+            get { return fileName; }
+            set
+            {
+                fileName = value;
+                if (fileName != string.Empty) Title = "Examiner " + VERSION + " - " + fileName;
+                else Title = "Examiner " + VERSION;
+            }
+        }
+        private string fileName = string.Empty;
+        private readonly string VERSION = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString() + " Beta";
+        #endregion
+
         public MainWindow()
         {
             InitializeComponent();
+            FileName = string.Empty;
+            this.Icon = Common.QuestionsAndAnswers.ToBitmapImage(Properties.Resources.Question);
         }
     }
 }
